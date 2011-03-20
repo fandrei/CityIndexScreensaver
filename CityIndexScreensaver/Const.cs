438f410f0a3fa0accab21b0f54ba449c6822e41f
fandrei <andrei_f_11@mail.ru>
2011-03-20 22:17:19 +0700

    screensaver config

---
+++ CityIndexScreensaver/Const.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CityIndexScreensaver
+{
+	static class Const
+	{
+		public const string AppName = "City Index Screensaver";
+	}
+}
